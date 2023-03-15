01575ed56556fcd9e34363afa05a8a550fec818e
Shreyansh <shreyansh.kumar32@outlook.com>
2023-03-15 18:11:57 +0530

    Project All Files Added

---
+++ demo/src/test/java/com/API_Crud_operation/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API_Crud_operation.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
